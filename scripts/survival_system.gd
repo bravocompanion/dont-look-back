b33ee69876b28537788987d7379c5b792942bdb8
bravocompanion <bravocompanion@outlook.com>
2026-08-15 20:56:12 +0800

    v0.4 add survival world setup

---
+++ scripts/survival_system.gd
@@ -0,0 +1,46 @@
+extends Node
+
+var configured_scene_id: int = 0
+var pickup_script: Script
+
+func _ready() -> void:
+    pickup_script = load("res://scripts/survival_pickup.gd") as Script
+
+func _process(_delta: float) -> void:
+    var scene: Node = get_tree().current_scene
+    if scene == null:
+        return
+
+    var scene_id: int = int(scene.get_instance_id())
+    if scene_id == configured_scene_id:
+        return
+
+    configured_scene_id = scene_id
+    call_deferred("_configure_scene", scene)
+
+func _configure_scene(scene: Node) -> void:
+    if not is_instance_valid(scene):
+        return
+
+    await get_tree().process_frame
+    if not is_instance_valid(scene) or get_tree().current_scene != scene:
+        return
+
+    _spawn_pickup(scene, "SurvivalFood", "canned_food", "Canned Food", Vector3(-4.55, 0.02, -4.15))
+    _spawn_pickup(scene, "SurvivalWater", "bottled_water", "Bottled Water", Vector3(-6.65, 0.02, -4.25))
+    _spawn_pickup(scene, "SurvivalMedkit", "medkit", "Medkit", Vector3(-6.95, 0.02, -6.55))
+
+func _spawn_pickup(scene: Node, node_name: String, item_id: String, display_name: String, world_position: Vector3) -> void:
+    if scene.has_node(node_name):
+        return
+    if pickup_script == null:
+        return
+
+    var pickup: StaticBody3D = StaticBody3D.new()
+    pickup.name = node_name
+    pickup.set_script(pickup_script)
+    pickup.set("item_id", item_id)
+    pickup.set("display_name", display_name)
+    pickup.set("objective_label_path", NodePath("../Player/HUD/Objective"))
+    pickup.position = world_position
+    scene.add_child(pickup)
