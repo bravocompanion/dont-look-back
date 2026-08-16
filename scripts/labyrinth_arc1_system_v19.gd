1449ef1698d8231c3ee98660e9162ce0f108782a
bravocompanion <bravocompanion@outlook.com>
2026-08-16 15:05:31 +0800

    remove unused Arc 1 RPC wrapper

--- scripts/labyrinth_arc1_system_v19.gd
+++
@@ -1,6 +0,0 @@
-extends "res://scripts/labyrinth_arc1_system.gd"
-
-@rpc("authority", "call_remote", "reliable", 8)
-func _receive_arc_state(state: Dictionary) -> void:
-    pending_restore_state = state.duplicate(true)
-    super._receive_arc_state(state)
