7f16c88af9e9d76ffa74cd6baaad136a46ca49db
bravocompanion <bravocompanion@outlook.com>
2026-08-16 18:37:46 +0800

    v0.21 update frontend build badge

--- scripts/front_end_system_v181.gd
+++ scripts/front_end_system_v181.gd
@@ -1,7 +1,7 @@
 extends "res://scripts/front_end_system.gd"
 
 const MAIN_MENU_SCENE_PATH: String = "res://scenes/main_menu.tscn"
-const VERSION_BADGE_TEXT: String = "v0.20  •  LABYRINTH MAJOR OVERHAUL"
+const VERSION_BADGE_TEXT: String = "v0.21  •  LABYRINTH EVACUATION & LIVING MAZE"
 var frontend_initialized: bool = false
 
 func _ready() -> void:
