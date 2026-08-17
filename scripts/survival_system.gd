a97b3068381017cd52f7402515cb0421ced7f0aa
bravocompanion <bravocompanion@outlook.com>
2026-08-17 18:59:35 +0800

    integrate forest survival runtime

--- scripts/survival_system.gd
+++ scripts/survival_system.gd
@@ -14,9 +14,10 @@ var narrative_lore_runtime: Node
 func _ready() -> void:
     pickup_script = load("res://scripts/survival_pickup.gd") as Script
     _attach_runtime("HudIconRuntime", "res://scripts/hud_icon_system.gd")
-    _attach_runtime("InventoryMenuRuntime", "res://scripts/inventory_menu_system.gd")
+    _attach_runtime("InventoryMenuRuntime", "res://scripts/inventory_menu_system_v25.gd")
     _attach_runtime("PanicMovementTuningRuntime", "res://scripts/panic_movement_tuning_system.gd")
     _attach_runtime("NarrativeLoreRuntime", "res://scripts/narrative_lore_system.gd")
+    _attach_runtime("ForestSurvivalRuntime", "res://scripts/forest_survival_system.gd")
 
 func _attach_runtime(node_name: String, script_path: String) -> void:
     if get_node_or_null(NodePath(node_name)) != null:
