3edca1117ab7f14f403812f0dd57de56f6b06787
bravocompanion <bravocompanion@outlook.com>
2026-08-22 13:29:08 +0800

    show v0.54 carry limits in inventory

--- scripts/survival_system_ranger_v11.gd
+++ scripts/survival_system_ranger_v11.gd
@@ -4,7 +4,7 @@ extends "res://scripts/survival_system_ranger_v2.gd"
 # balance changes for carry limits, progression, and renewable-resource pressure.
 func _ready() -> void:
     pickup_script = load("res://scripts/survival_pickup.gd") as Script
-    _attach_runtime("InventoryMenuRuntime", "res://scripts/inventory_menu_system_v43.gd")
+    _attach_runtime("InventoryMenuRuntime", "res://scripts/inventory_menu_system_v54.gd")
     _attach_runtime("PanicMovementTuningRuntime", "res://scripts/panic_movement_tuning_system.gd")
     _attach_runtime("NarrativeLoreRuntime", "res://scripts/narrative_lore_system.gd")
     _attach_runtime("ForestSurvivalRuntime", "res://scripts/forest_survival_system_v54.gd")
